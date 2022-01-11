f340cfbb19d82dbe1b29bca8150ad71fa0a44641
kkaishu <90711409+kkaishu@users.noreply.github.com>
2022-01-11 14:15:00 +0900

    Update youtubemp3.py

--- youtubemp3.py
+++ youtubemp3.py
@@ -3,18 +3,18 @@ import streamlit as st
 
 st.title('youtube→mp3変換プログラム')
 st.header('下に変換したいyoutubeのURLを張り付けてください。もしかしたら時間がかかるかもです。')
-url=input()
 
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'outtmpl':  "sample_music" + '.%(ext)s',
-    'postprocessors': [
-        {'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-         'preferredquality': '192'},
-        {'key': 'FFmpegMetadata'},
-    ],
-}
 
-ydl = youtube_dl.YoutubeDL(ydl_opts)
-info_dict = ydl.extract_info(url, download=True)
+# ydl_opts = {
+#     'format': 'bestaudio/best',
+#     'outtmpl':  "sample_music" + '.%(ext)s',
+#     'postprocessors': [
+#         {'key': 'FFmpegExtractAudio',
+#         'preferredcodec': 'mp3',
+#          'preferredquality': '192'},
+#         {'key': 'FFmpegMetadata'},
+#     ],
+# }
+
+# ydl = youtube_dl.YoutubeDL(ydl_opts)
+# info_dict = ydl.extract_info(url, download=True)
