ce8a2eddbb7793c0a61d394197c59e698d5dc234
kkaishu <90711409+kkaishu@users.noreply.github.com>
2022-01-11 13:55:39 +0900

    Create youtubemp3.py

---
+++ youtubemp3.py
@@ -0,0 +1,18 @@
+pip install youtube-dl
+import youtube_dl
+
+url=input()
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl':  "sample_music" + '.%(ext)s',
+    'postprocessors': [
+        {'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+         'preferredquality': '192'},
+        {'key': 'FFmpegMetadata'},
+    ],
+}
+
+ydl = youtube_dl.YoutubeDL(ydl_opts)
+info_dict = ydl.extract_info(url, download=True)
