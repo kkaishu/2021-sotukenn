c45600543e51b7a2771d8b6f8386d62be9e044c6
kkaishu <90711409+kkaishu@users.noreply.github.com>
2022-01-11 14:11:02 +0900

    Update youtubemp3.py

--- youtubemp3.py
+++ youtubemp3.py
@@ -1,7 +1,7 @@
 import youtube_dl
 import streamlit as st
 
-st.title('youtubeからmp3ファイルに変換してダウンロードするプログラムです。'）
+st.title('J2リーグ観客動員数予測プログラム'）
 st.header('下に変換したいyoutubeのURLを張り付けてください。もしかしたら時間がかかるかもです。')
 url=input()
 
