6a064de49912cc1c7447e12667bdf32f1528fc1b
kkaishu <90711409+kkaishu@users.noreply.github.com>
2022-01-11 14:27:03 +0900

    Create youtube_mp3.py

---
+++ youtube_mp3.py
@@ -0,0 +1,22 @@
+import youtube_dl
+import streamlit as st
+
+st.title('youtube→mp3変換プログラム')
+st.header('下に変換したいyoutubeのURLを張り付けてください。もしかしたら時間がかかるかもしれないです。')
+# inputbox
+url= st.text_input('inputbox', 'おはよう')
+st.write('inputbox:', url)
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl':  "sample_music" + '.%(ext)s',
+    'postprocessors': [
+        {'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+         'preferredquality': '192'},
+        {'key': 'FFmpegMetadata'},
+    ],
+}
+
+ydl = youtube_dl.YoutubeDL(ydl_opts)
+info_dict = ydl.extract_info(url, download=True)
